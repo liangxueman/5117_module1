5901bbfbcf019a05058927ced143004e294c2273
liang195 <liang195@umn.edu>
2018-10-07 20:16:26 -0500

    remove app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-	# foo = request.args["foo"]
-	app.logger.info("logger text")
-	return render_template("foo.html", arg="foo")
